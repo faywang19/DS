98b2d06826c82c768d46d47697c9413b69d0d2df
faywang19 <faywang19@163.com>
2021-01-06 00:45:10 +0800

    second commit

--- src/ct/test/Main.java
+++ src/ct/test/Main.java
@@ -3,5 +3,7 @@ package ct.test;
 public class Main {
     public static void main(String[] args) {
         System.out.println("当你看到这句话意味着更新成功了，是第二次提交");
+        System.out.println("什么鬼");
+        System.out.println("为什么我没有看到第二次修改的呢");
     }
 }
